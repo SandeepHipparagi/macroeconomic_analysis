dbc180c5881b2c425be993a6b5a8d2111aafa0b2
SandeepHipparagi <83487835+SandeepHipparagi@users.noreply.github.com>
2023-08-30 21:30:34 +0530

    MEA_Data_Collection.py

--- MEA_Data_Collection.py
+++ MEA_Data_Collection.py
@@ -1,26 +1,13 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import requests
 
-# Example URL for data source
+# URL for data source
 data_source_url = "https://datacatalog.worldbank.org/dataset/worlddevelopment-indicators"
 
 # Fetch data from the source
 response = requests.get(data_source_url)
-data = response.json()  # Assuming the data is in JSON format
+data = response.json() 
 
 # Save data to a CSV file
 with open("macroeconomic_data.csv", "w") as f:
     for entry in data:
         f.write(",".join(entry.values()) + "\n")
-
-
-# In[ ]:
-
-
-
-
